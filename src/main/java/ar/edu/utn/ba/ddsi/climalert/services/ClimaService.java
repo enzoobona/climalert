928ec8a8656cdd3a1979ad12e6ff04bc4b1f2327
enzoobona <ebonafine@frba.utn.edu.ar>
2026-06-29 02:17:30 -0300

    feature(services): Agrego servicio de clima

--- src/main/java/ar/edu/utn/ba/ddsi/climalert/services/ClimaService.java
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/services/ClimaService.java
@@ -3,5 +3,7 @@ package ar.edu.utn.ba.ddsi.climalert.services;
 import ar.edu.utn.ba.ddsi.climalert.dtos.Clima;
 
 public interface ClimaService {
-    public Clima obtenerTemperaturaYHumedad();
+    Clima obtenerClima();
+
+    void analizarClimas();
 }
--- src/main/java/ar/edu/utn/ba/ddsi/climalert/services/impl/ClimaServiceImpl.java
+++ src/main/java/ar/edu/utn/ba/ddsi/climalert/services/impl/ClimaServiceImpl.java
@@ -2,8 +2,12 @@ package ar.edu.utn.ba.ddsi.climalert.services.impl;
 
 import ar.edu.utn.ba.ddsi.climalert.config.RestWeatherProperties;
 import ar.edu.utn.ba.ddsi.climalert.dtos.Clima;
+import ar.edu.utn.ba.ddsi.climalert.dtos.ClimaActual;
+import ar.edu.utn.ba.ddsi.climalert.repositories.ClimaRepository;
+import ar.edu.utn.ba.ddsi.climalert.services.AlertaService;
 import ar.edu.utn.ba.ddsi.climalert.services.ClimaService;
 import java.net.URI;
+import java.util.Optional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,14 +17,18 @@ public class ClimaServiceImpl implements ClimaService {
 
     private RestTemplate restTemplate;
     private RestWeatherProperties properties;
+    private ClimaRepository climaRepository;
+    private AlertaService alertaService;
 
-    public ClimaServiceImpl(RestTemplate restTemplate, RestWeatherProperties properties) {
+    public ClimaServiceImpl(RestTemplate restTemplate, RestWeatherProperties properties, ClimaRepository climaRepository, AlertaService alertaService) {
         this.restTemplate = restTemplate;
         this.properties = properties;
+        this.climaRepository = climaRepository;
+        this.alertaService = alertaService;
     }
 
     @Override
-    public Clima obtenerTemperaturaYHumedad() {
+    public Clima obtenerClima() {
         URI uri = UriComponentsBuilder
             .fromUriString(properties.getBaseUrl())
             .path(properties.getMethod())
@@ -31,7 +39,34 @@ public class ClimaServiceImpl implements ClimaService {
 
         Clima clima = restTemplate.getForObject(uri, Clima.class);
 
+        this.climaRepository.save(clima);
+
         return clima;
+    }
+
+    @Override
+    public void analizarClimas() {
+
+        String alerta = "Alerta: ";
+
+        Optional<Clima> ultimo = this.climaRepository.findLast();
+        ClimaActual clima = ultimo.get().getClimaActual();
+
+        alerta += temperaturaAlta(clima.getTemperatura());
+        if(!alerta.equals("Alerta: ")) alerta += " y ";
+        alerta += humedadAlta(clima.getHumedad());
+        alerta += ".";
+
+        this.alertaService.enviarAlerta(alerta);
+
+    }
+
+    private String temperaturaAlta(double temperatura) {
+        return temperatura > 35.0 ? "Temperatura de " + temperatura + " grados " : "";
+    }
+
+    private String humedadAlta(int humedad) {
+        return humedad > 60 ? "Humedad de " + humedad + " % " : "";
     }
 
 }
